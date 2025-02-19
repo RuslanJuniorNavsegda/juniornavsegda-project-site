6f86dd0f335a28b47a0ba0b85b21e27186cecc49
Ruslan <kanii.ues@gmail.com>
2025-02-19 13:34:24 +0300

    new structure

---
+++ script.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate");
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+document.querySelectorAll(".animate").forEach((el) => {
+  observer.observe(el);
+});
